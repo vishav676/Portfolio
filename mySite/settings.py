6d1361df82a892d50b923e85d0b58397726caeea
Vishav Lakhtia <vishav676@gmail.com>
2021-01-12 22:40:34 +0100

    Initial Commit

--- mySite/settings.py
+++ mySite/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3m%#+$5ixv1kujz56%@-jpgdlj2m-d=cmd8w&5@ue$n#q%%g@v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://vishav676.herokuapp.com/','127.0.0.1']
 
 # Application definition
 
